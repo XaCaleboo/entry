f502aa143573917794b177dae6a80f6a6c50cbe4
Dmitry Lekomtsev <dolf99@yandex.ru>
2018-04-29 18:31:21 +0400

    Добавил интерактивчика

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import Form from './Form';
-import Pay from './Pay'
-
+import StyledForm from './StyledForm';
+import Pay from './Pay';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+const Main = () => (
+    <main>
+      <Switch>
+        <Route exact path='/' component={StyledForm}/>
+        <Route path='/pay' component={Pay}/>
+      </Switch>
+    </main>
+  )
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Form />
+                <Main/>
             </div>
         );
     }
